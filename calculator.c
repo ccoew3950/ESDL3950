9694cd4650ee0891b0f368eb86379d75a241e016
ccoew3950 <pragati.nevhal@cumminscollege.in>
2016-08-08 16:54:30 +0530

    Update calculator.c
    included add() function

--- calculator.c
+++ calculator.c
@@ -1,7 +1,16 @@
 this is my first file in github.
+
+
 #include <stdio.h>
 void main()
 {
 int a,b,c;
 //Add calculator function here
+//Add functionalities like addition ,subtraction etc.
+
+int add(int p,int q)
+{
+  return a+b;
+}
+
 }
